50521093b519a924c24436afdd2e63f7f4fd693d
cens_a <ejungwoo@ibs.re.kr>
2025-05-17 09:46:15 +0000

    Reaction 34Ar(a,p)37K is created from Actar

--- macros/sim_run.sh
+++ macros/sim_run.sh
@@ -1,1 +1,5 @@
-npsimulation -D Example4.detector -E Example4.reaction --random-seed 20250517 -O Example4_out -B run.mac
+npsimulation --detector ATOMX.detector\
+             --event-generator ATOMX_34Ar_alpha_p.reaction\
+             --random-seed 20250517\
+             --output ATOMX_simulation\
+             -B geant4_run.mac
--- macros/sim_viewer.sh
+++ macros/sim_viewer.sh
@@ -1,1 +1,2 @@
 npsimulation -D ATOMX.detector -E ATOMX_34Ar_alpha_p.reaction
+#npsimulation -D ATOMX.detector -E Example4.reaction
